2c4a47120c6ed3573c7b418f632c6b37580800f9
Brandon Blinderman <brandon@bblinder>
2015-04-18 14:47:46 -0400

    new file:   Current_IP_Addr 	new file:   MP3_convert.sh

---
+++ MP3_convert.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+mp3_convert(){
+  for fname in *.m4a ; do
+    ffmpeg -i "$fname" -c:a libmp3lame -b:a 320k "${fname%.*}.mp3"
+
+  done
+
+}
+
+if [[ -e "${fname%.*}.mp3" ]]
+  then
+  echo "'${fname%.*}.mp3' already exists!"
+  echo "Do you want to overwite it? [y/n]"
+
+  read response
+
+  if [[ $response == "y" ]]
+    then
+    mp3_convert
+    else
+    exit 1
+  fi
+
+  else
+      mp3_convert
+fi
+
+if [[ -e "$fname" ]]
+  then
+  echo "Do you want to delete the originals? [y/n]"
+  read response
+
+  if [[ $response == "y" ]]
+    then
+        rm *.m4a
+        echo "Deleting..."
+        sleep 1
+        exit 0
+    else
+        exit 0
+    fi
+fi
+
+##written by Brandon Blinderman 2015
