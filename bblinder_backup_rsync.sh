fe68ab341eff49fd597aeed5b9f0be989218a2cd
Brandon Blinderman <brandon@bblinder>
2015-05-10 13:48:14 -0400

    modified:   bblinder_backup_rsync.sh

--- bblinder_backup_rsync.sh
+++ bblinder_backup_rsync.sh
@@ -1,7 +1,7 @@
 #!/bin//bash
 
 SYNC(){
-    rsync -e ssh -avh --progress --delete --inplace --compress-level=0 ~/BackUps/bblinder_backup-$(date +%F).tar.gz pi@192.168.0.7:/media/GooseHaus1/shares/.bblinder_backup/ ; return 0
+    rsync -e ssh -avh --progress --delete --inplace --compress-level=0 ~/BackUps/bblinder_backup-$(date +%F).tar.gz pi@raspberrypi:/path/to/.bblinder_backup/ ; return 0
 
 }
 
