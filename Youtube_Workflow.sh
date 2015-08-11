753df545d852421f6475b8841fb099be7dae9798
bblnder <brandon@BlindySupport.local>
2015-08-11 14:26:43 -0400

    Syntax changes

--- Youtube_Workflow.sh
+++ Youtube_Workflow.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 convert='~/Github/home-brews/MP3_convert.sh'
 youtube-dl -x $1 ; . $convert 
 
-if [[ ! -e $1 ]]
-then
+if [[ ! -e $1 ]] ; then
 	echo "Provide a link, please."
 	exit
 fi
