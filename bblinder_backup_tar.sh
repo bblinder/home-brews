1a51fe74883e08f9abed5090724d1af20681a546
bblnder <brandon@BlindySupport.local>
2015-08-18 11:33:35 -0400

    Syntax, baby, syntax

--- bblinder_backup_tar.sh
+++ bblinder_backup_tar.sh
@@ -5,15 +5,17 @@ cd /tmp/
 echo "Compressing..."
 
 COMPRESS(){
-    tar -cpzf bblinder_backup-$(date +%F).tar.gz ~/Documents ~/Downloads ~/.gnupg ~/.irssi ~/Music/MP3_convert.sh ~/Pictures ~/.ssh ~/.bashrc ~/.zshrc ~/Videos /etc/vim/vimrc ~/.config/terminator/config ; return 0 || return 1
-
+    tar -cpzf bblinder_backup-$(date +%F).tar.gz ~/Documents ~/Downloads ~/.gnupg\
+	    ~/.irssi ~/Music/MP3_convert.sh ~/Pictures ~/.ssh ~/.bashrc\
+	    ~/.zshrc ~/Videos /etc/vim/vimrc ~/.config/terminator/config ; return 0 || return 1
 }
 
 COMPRESS
 
 if [[ $COMPRESS -eq 0 ]] ; then
 	mv /tmp/bblinder_backup-$(date +%F).tar.gz ~/BackUps
-	echo "All done. File size is $(du -sh ~/BackUps/bblinder_backup-$(date +%F).tar.gz | awk '{ print $1 }')"
+	echo "All done. File size is $(du -sh ~/BackUps/bblinder_backup-$(date +%F).tar.gz\
+	       	| awk '{ print $1 }')"
 else
 	echo "Compression Failed."
 fi
