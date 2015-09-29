0f1ba7a70a4bb1e1404d2be6966ca39ef2fd0756
bblnder <brandon@BlindySupport.local>
2015-09-29 14:58:07 -0400

    Added ffmpeg

--- caskfile.sh
+++ caskfile.sh
@@ -14,7 +14,7 @@ fi
 brew_array=(archey axel curl exiftool csshx cowsay htop icdiff\
 	irssi lynx mplayer mtr netcat osxfuse p7zip pstree sshfs\
 	speedtest_cli pv rtmpdump tldr wget youtube-dl zsh git\
-	gnu-units lnav ranger watch iftop)
+	gnu-units lnav ranger watch iftop ffmpeg)
 
 # Casks
 cask_array=(alfred flux ccleaner virtualbox google-chrome firefox\
