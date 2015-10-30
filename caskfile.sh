a249e895c0dfda2bbcf3b350bdcb535f532ecd0c
bblnder <brandon@BlindySupport.local>
2015-10-30 13:00:08 -0400

    Removed Livestream Producer

--- caskfile.sh
+++ caskfile.sh
@@ -19,7 +19,7 @@ brew_array=(archey axel curl exiftool csshx cowsay htop icdiff\
 # Casks
 cask_array=(alfred flux ccleaner virtualbox google-chrome firefox\
 	vivaldi iterm2 adium slack skype sublime-text atom xquartz\
-	spotify beardedspice livestream-producer gpgtools vlc wireshark\
+	spotify beardedspice the-unarchiver gpgtools vlc wireshark\
 	spectacle github gfxcardstatus superduper chrome-remote-desktop-host\
 	handbrake teamviewer carbon-copy-cloner)
 
