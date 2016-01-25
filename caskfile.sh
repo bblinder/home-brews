f33219db5f56eedfe26340d9161991fdd91030bb
bblinder <brandon.blinderman@gmail.com>
2016-01-25 15:33:16 -0500

    Added malwarebytes

--- caskfile.sh
+++ caskfile.sh
@@ -22,7 +22,7 @@ cask_array=(alfred flux ccleaner virtualbox google-chrome firefox\
 	vivaldi iterm2 adium slack skype sublime-text atom xquartz\
 	spotify beardedspice the-unarchiver gpgtools vlc wireshark\
 	spectacle github-desktop gfxcardstatus superduper chrome-remote-desktop-host\
-	handbrake teamviewer carbon-copy-cloner clipmenu etrecheck)
+	handbrake teamviewer carbon-copy-cloner clipmenu etrecheck malwarebytes-anti-malware)
 
 INSTALL_STUFF(){
 	# Install HomeBrew
