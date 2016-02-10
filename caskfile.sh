800a18f0de2257f44d1388063f24421679a56828
bblinder <brandon.blinderman@gmail.com>
2016-02-10 12:22:43 -0500

    added 'Ack'

--- caskfile.sh
+++ caskfile.sh
@@ -11,7 +11,7 @@ fi
 # Or via ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Unix utilities
-brew_array=(archey axel curl exiftool csshx cowsay htop icdiff\
+brew_array=(archey ack axel curl exiftool csshx cowsay htop icdiff\
 	irssi lynx mplayer mtr osxfuse p7zip pstree sshfs\
 	speedtest_cli pv rtmpdump tldr wget youtube-dl zsh git\
 	gnu-units lnav ranger watch iftop ffmpeg ssh-copy-id spoof-mac\
