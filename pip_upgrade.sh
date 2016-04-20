3fa1824205c17cf5a9fbc2d6833e37e599eb04f8
bblinder <brandon.blinderman@gmail.com>
2016-04-20 10:08:58 -0400

    Added Haxor News

--- pip_upgrade.sh
+++ pip_upgrade.sh
@@ -16,7 +16,7 @@ general_packages(){
 choice_packages(){
 	pip list | awk '{ print $1 }' | egrep -i "(pip)|(livestreamer)|(youtube-dl)|\
     (thefuck)|(tldr)|(zenmap)|(paramiko)|(clf)|(Fabric)|\
-    (speedtest-cli)|(setuptools)|(ohmu)|(httpie)|(stormssh)" > "$LIST" 
+    (speedtest-cli)|(setuptools)|(ohmu)|(haxor-news)|(httpie)|(stormssh)" > "$LIST" 
 }
 
 pip_upgrade(){
