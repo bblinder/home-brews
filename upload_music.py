2c3f24c4eaf48a2ca81ecf742e20cd61ea42f010
bblinderman <bblinderman@freewheel.tv>
2016-07-01 12:17:59 -0400

    modified:   upload_music.py

--- upload_music.py
+++ upload_music.py
@@ -3,7 +3,13 @@
 # Simple script to upload mp3's to my Google Music account. Uses the unofficial gmusic API.
 
 from sys import argv
-from gmusicapi import Musicmanager
+
+try:
+	from gmusicapi import Musicmanager
+except ImportError:
+	print "Can't import gmusicapi - check that it's installed"
+	print "Exiting..."
+	sys.exit()
 
 mm = Musicmanager()
 #mm.perform_oauth() # Should be run for the first implementation only. Not needed afterwards.
