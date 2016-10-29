9215b61847d7162fdf11263944f45d285dafe0b8
bblinderman <bblinderman@freewheel.tv>
2016-10-29 13:19:06 -0400

    modified:   disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Temporarily disables IPv6. Used with my OpenVPN config.
+# Temporarily disables IPv6. Used with my OpenVPN config on Debian 8.
 
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
