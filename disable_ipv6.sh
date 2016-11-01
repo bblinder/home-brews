6eb6b45115078a93be4f61e71069103c9e81b807
bblinderman <bblinderman@freewheel.tv>
2016-11-01 10:42:06 -0400

    "Added OS check."

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -2,4 +2,9 @@
 
 # Temporarily disables IPv6. Used with my OpenVPN config on Debian 8.
 
+if [[ $(uname -s) != "Linux" ]] ; then
+	echo "ERROR: This script will only run on Linux systems."
+	exit 1
+fi
+
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
