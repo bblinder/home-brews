a50ce5bc071324495863725ee3616038f4b3c8cc
bblinderman <bblinderman@freewheel.tv>
2016-11-16 09:45:41 -0500

    modified:   disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -5,6 +5,8 @@
 if [[ $(uname -s) != "Linux" ]] ; then
 	echo "ERROR: This script will only run on Linux systems."
 	exit 1
+else
+	sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 fi
 
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1 ; sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
