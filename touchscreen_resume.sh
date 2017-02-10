aa6a456f764f9b4e40c9af9b74e39203f4274cc9
bblinderman <bblinderman@freewheel.tv>
2017-02-10 10:06:18 -0500

    Added OS check.

--- touchscreen_resume.sh
+++ touchscreen_resume.sh
@@ -3,4 +3,9 @@
 # Fixes the intermittent touchscreen issue on my XPS 13 running Debian Jessie.
 # Script stored in /opt/touchscreen-fix
 
-/sbin/modprobe -r hid_multitouch && /sbin/modprobe hid_multitouch
+if [[ $(uname -s) != "Linux" ]] ; then
+	echo "ERROR: This script will only run on Linux systems."
+	exit 1
+else
+	/sbin/modprobe -r hid_multitouch && /sbin/modprobe hid_multitouch
+fi
