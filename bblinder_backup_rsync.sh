035f5880a987d60d6575ffb7ea110d8d7c70eb95
bblinder <filmmaven@gmail.com>
2017-04-14 16:15:27 -0400

    modified:   bblinder_backup_rsync.sh

--- bblinder_backup_rsync.sh
+++ bblinder_backup_rsync.sh
@@ -1,14 +1,25 @@
 #!/bin//bash
 
+set -euo pipefail
+#unofficial bash 'strict mode'
+IFS=$'\n\t'
+
+# Assumes you've encrypted with GPG.
+FILE_TO_TRANSFER="$HOME/path/to/backup-$(date +%F).tar.gz.gpg"
+
 SYNC(){
-    rsync -e ssh -avh --progress --delete --inplace --compress-level=0 ~/BackUps/ \
-	    bblinder_backup-$(date +%F).tar.gz pi@raspberrypi:/path/to/.bblinder_backup/ ; return 0 \
-	    || return 1
+    rsync -e ssh -avh --progress --compress-level=0 "$FILE_TO_TRANSFER" \
+	    pi@[ip_address]:/path/to/backup/folder || return 1
 }
 
-SYNC
+if [[ -e "$FILE_TO_TRANSFER" ]] ; then
+	SYNC
+else
+	echo "File to transfer not found."
+	exit 1
+fi
 
-if [[ $SYNC -eq 0 ]]; then
+if [[ SYNC ]] ; then
 	echo "All done."
 else
     echo "Sync failed."
