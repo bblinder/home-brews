d3874cbcca486cfdaff539bfe74a58f63583aa22
bblinder <filmmaven@gmail.com>
2017-04-14 16:26:28 -0400

    modified:   bblinder_backup_tar.sh

--- bblinder_backup_tar.sh
+++ bblinder_backup_tar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -euo pipefail # unofficial bash 'strict mode'
+set -euo pipefail 
+# unofficial bash 'strict mode'
 IFS=$'\n\t'
 
 BACKUP_FILE="bblinder_backup-$(date +%F).tar.gz"
 BACKUP_DIR="$HOME/BackUps"
+FILE_TO_ENCRYPT="$BACKUP_DIR/$BACKUP_FILE"
 
 cd /tmp/ && echo "Compressing..."
 
@@ -24,3 +26,18 @@ else
 	echo "Compression Failed."
 fi
 
+ENCRYPT(){
+	if [[ "$FILE_TO_ENCRYPT" ]] ; then
+		echo "Encrypting... standby..."
+		sudo gpg --symmetric "$FILE_TO_ENCRYPT" || return 1
+	fi
+}
+
+ENCRYPT
+
+if [[ ENCRYPT ]] ; then
+	echo "Encryption done... ready for transfer."
+	rm "$FILE_TO_ENCRYPT"
+else
+	echo "Encryption failed."
+fi 
