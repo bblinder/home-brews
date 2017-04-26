971871a0647afc341a16421daa5239ade8a17e56
bblinder <filmmaven@gmail.com>
2017-04-26 10:14:06 -0400

    "Added Git-Up"

--- user_installed_crap.sh
+++ user_installed_crap.sh
@@ -63,7 +63,7 @@ ruby_upgrade(){
 bulk_git_update(){
 	Github="$HOME/Github"
 	if [[ -e /usr/local/bin/git-up ]] ; then
-		for dir in "$Github"/* ; do (cd "$dir" && && git remote update && git up) ; done
+		for dir in "$Github"/* ; do (cd "$dir" && git remote update && git up) ; done
 	else
 		for dir in "$Github"/* ; do (cd "$dir" && git remote update && git pull --rebase && git gc --auto); done
 	fi
