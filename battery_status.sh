67a3266c6e218aa277d06a4f9a80c6b0460a1ea2
bblinder <filmmaven@gmail.com>
2017-04-28 17:06:36 -0400

    Adding Battery Status Script

--- battery_status.sh
+++ battery_status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# A script to trigger a music file if battery level falls below 20%
+# A script to trigger a music file if battery level falls below 20%.
+# Mac OS X only
 
 # Bash "strict" mode
 set -euo pipefail
@@ -8,6 +9,12 @@ set -euo pipefail
 battery_level="$(pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f1 -d';' | sed -e 's/%//')"
 
 music_file=/path/to/file
+
+
+if [[ "$(uname -s)" != "Darwin" ]] ; then
+	echo "This will only run on Mac OS X."
+	exit 1
+fi
 
 if [[ "$battery_level" -lt 20 ]] ; then
 	if [[ -e /usr/local/bin/noti ]] ; then
