5e8c0dfcfa782a5abd9cb77698615b9eea92c2a1
Brandon Blinderman <brandon@bblinder>
2017-04-29 14:36:58 -0400

    new file:   disable_suspend.sh

---
+++ disable_suspend.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+linux_version=$(cat /etc/*-release | grep -i "version=" | awk '{ print $1 }' | sed -e 's/"//g')
+
+if [[ "$(uname -s)" != "Linux" ]] ; then
+    echo "::: This script will only run on Linux systems"
+    exit 1
+elif 
+    [[ "$linux_version" != "VERSION=8" ]] ; then
+    echo "::: This script will only run on systems with systemd (Debian Jessie, etc.)"
+    exit 1
+fi
+
+disable_suspend(){
+    sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+}
+
+enable_suspend(){
+    sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target
+}
+
+read -rp "Disable (1) or Enable (2) Suspend? >  " SUSPEND_RESPONSE
+
+case "$SUSPEND_RESPONSE" in
+    1)
+        disable_suspend
+        ;;
+    2)
+        enable_suspend
+        ;;
+    *)
+        echo "Please enter (1) or (2)"
+        ;;
+esac
