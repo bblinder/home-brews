9a4cbf884ae900db4db0bfa942ee0aaa1a4788f9
bblinder <filmmaven@gmail.com>
2017-06-26 18:56:23 -0400

    new file:   send_sms_smtp.py 	new file:   send_sms_twilio.py 	deleted:    send_sms.py

---
+++ send_sms_twilio.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import random
+try:
+    from twilio.rest import Client
+except ImportError:
+    print "Can't import Twilio -- check that it's installed"
+    print "Example: 'pip -u install twilio'"
+    sys.exit(1)
+
+# assumes you have a 'quotes.txt' file living in your directory
+result = (random.choice(list(open('quotes'))))
+
+# these are found in the Twilio dashboard
+account_sid = ""
+auth_token = ""
+
+client = Client(account_sid, auth_token))
+
+client.messages.create(to="",
+        from_="",
+        body=result)
+
