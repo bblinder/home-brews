ea408fc19fae91c1ef920d97bff250f279ae3cac
bblinder <filmmaven@gmail.com>
2017-08-02 10:10:10 -0400

    new file:   showerthoughts_motd.sh

---
+++ showerthoughts_motd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Stolen and modifed from a reddit thread.
+
+username="" # your reddit handle
+
+showerthoughts=$(curl -s --connect-timeout 5 -A '$username' \
+'https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=week&limit=100' | \
+python2.7 -c 'import sys, random, json; randnum = random.randint(0,99); response = json.load(sys.stdin)["data"]["children"][randnum]["data"]; print "\n\"" + response["title"] + "\""; print "    -" + response["author"] + "\n";')
+
+echo $showerthoughts | cowsay | lolcat
