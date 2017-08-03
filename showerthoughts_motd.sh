c20cd00560a930fa8ef41d1379aac642b20c1aa8
bblinder <filmmaven@gmail.com>
2017-08-03 13:30:33 -0400

    modified:   showerthoughts_motd.sh

--- showerthoughts_motd.sh
+++ showerthoughts_motd.sh
@@ -6,8 +6,7 @@ set -euo pipefail
 
 username="" # your reddit handle
 
-showerthoughts=$(curl -s --connect-timeout 5 -A '$username' \
-'https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=week&limit=100' | \
+showerthoughts=$(http 'https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=week&limit=100' | \
 python2.7 -c 'import sys, random, json; randnum = random.randint(0,99); response = json.load(sys.stdin)["data"]["children"][randnum]["data"]; print "\n\"" + response["title"] + "\""; print "    -" + response["author"] + "\n";')
 
 echo $showerthoughts | cowsay | lolcat
