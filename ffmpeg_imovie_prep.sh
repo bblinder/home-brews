a06e6dfdce5a5ec73cc3d692ab88099bfb24d52f
bblinder <filmmaven@gmail.com>
2018-01-02 14:12:56 -0500

    modified:   ffmpeg_imovie_prep.sh

--- ffmpeg_imovie_prep.sh
+++ ffmpeg_imovie_prep.sh
@@ -2,19 +2,29 @@
 
 # using this to prep MPEG 1/2 multiplexed files for editing and viewability in QT and iMovie.
 
+set -eou pipefail
+IFS=$'\n\t'
+shopt -s nullglob
+
 ffmpeg_check(){
-	if [[ ! ( -f /usr/bin/ffmpeg || -f /usr/local/bin/ffmpeg ) ]] ; then
-		echo "FFmpeg not installed!"
-		echo "Please install it at: https://ffmpeg.org/download.html"
-		exit 1
-	fi
+	type ffmpeg >/dev/null 2>&1 || { echo >$2 "::: ffmpeg not installed. Please install it at: https://ffmpeg.org/download.html" ; exit 1; }
 }
+
+# An older check; leaving it here in case the newer method breaks compatibility.
+#ffmpeg_check(){
+#	if [[ ! ( -f /usr/bin/ffmpeg || -f /usr/local/bin/ffmpeg ) ]] ; then
+#		echo "FFmpeg not installed!"
+#		echo "Please install it at: https://ffmpeg.org/download.html"
+#		exit 1
+#	fi
+#}
+
 ffmpeg_single(){
-	while read x ; do
+	while read -r x ; do
 		ffmpeg -i "${x}" -strict experimental -pix_fmt yuv420p \
 		-c:v libx264 -c:a aac \
 		-ab 160000 -preset slow \
-		-crf 15 "${x/%.*/_imovie.mp4}"
+		-crf 15 "${x/%.*/_imovie.mp4}" < /dev/null
 	done
 }
 DEAL_WITH_IT(){
@@ -37,11 +47,12 @@ ffmpeg_batch(){
 	for x in *.mp4; do
 		ffmpeg -i "${x}" -strict experimental \
 		-pix_fmt yuv420p -vcodec libx264 -acodec aac \
-    -ab 160000 -preset slow \
-    -crf 18 "${x/.mp4/_imovie.mp4}";
+		-ab 160000 -preset slow \
+		-crf 18 "${x/.mp4/_imovie.mp4}"
 	done
 	exit 0
 }
+EOL
 
 ffmpeg_check
 read -rp "Enter (1) to transcode a single file, or (2) to transcode a folder -->   " TRANSCODE_CHOICE
