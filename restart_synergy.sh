23e75ccbc70378fa671aa50a97283e426f20c185
bblinder <filmmaven@gmail.com>
2018-01-06 13:15:59 -0500

    Some minor changes

--- restart_synergy.sh
+++ restart_synergy.sh
@@ -3,14 +3,21 @@
 # quick and dirty script to restart synergy on my OS's.
 
 set -euo pipefail
+IFS=$'\n\t'
+
+if [[ "$(id -u)" -ne 0 ]] ; then
+	echo "::: Please run as root."
+	exit
+fi
 
 if [[ "$(uname -s)" == "Darwin" ]] ; then 
 	launchctl unload /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
-	sudo killall synergy-core &
+	killall synergy-core &
+	sleep 0.5
 	launchctl load /Library/LaunchAgents/com.symless.synergy.synergy-service.plist
 	open -n /Applications/Synergy.app
 elif
 	[[ "$(uname -s)" == "Linux" ]] ; then
-	sudo systemctl restart synergy
+	systemctl restart synergy
 fi
 
