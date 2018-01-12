6356074bae3ac4141127f5fdf6d3f78437b19ebf
bblinder <filmmaven@gmail.com>
2018-01-12 17:46:06 -0500

    modified:   Debian_Blindy.sh

--- Debian_Blindy.sh
+++ Debian_Blindy.sh
@@ -110,7 +110,7 @@ INSTALL_DEB_PERSONALS(){
 	#wget "$Caprine"
 
 	if [[ "$(command -v axel)" ]] ; then
-		axel -an 5 "${deb_array[@]}" || wget "${deb_array[@]}"
+		axel -an 5 "${deb_array[@]}"
 	else
 		wget "${deb_array[@]}"
 	fi
