58209ebe842ec3d448a9bea1586a34c9bc56663d
bblinder <filmmaven@gmail.com>
2018-03-02 12:48:30 -0500

    modified:   pip_upgrade.sh

--- pip_upgrade.sh
+++ pip_upgrade.sh
@@ -7,22 +7,22 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-LIST='/tmp/pip_list.txt' # Where we're temporarily keeping our stuff.
+LIST='/tmp/pip2_list.txt' # Where we're temporarily keeping our stuff.
 
 general_packages(){
-	pip list | awk '{ print $1 }' | sed -e 's/-//g' -e 's/youtubedl/youtube-dl/g' -e '/^\s*$/d' | tail -n +2 > "$LIST"
+	pip2 list | awk '{ print $1 }' | sed -e 's/-//g' -e 's/youtubedl/youtube-dl/g' -e '/^\s*$/d' | tail -n +2 > "$LIST"
 }
 
 choice_packages(){
-	pip list | awk '{ print $1 }' | grep -Ei "pip|livestreamer|youtube-dl|\
+	pip2 list | awk '{ print $1 }' | grep -Ei "pip2|livestreamer|youtube-dl|\
 		thefuck|tldr|zenmap|paramiko|clf|Fabric|\
 		speedtest-cli|setuptools|ohmu|haxor-news|httpie|stormssh|waybackpack|\
 		http-prompt|glances|musicrepair" > "$LIST" 
 }
 
-pip_upgrade(){
+pip2_upgrade(){
     while read -r package; do
-        sudo -H pip install "$package" --upgrade
+        sudo -H pip2 install "$package" --upgrade
     done < "$LIST" || return 1
 }
 
@@ -35,18 +35,18 @@ read -rp "General update (1) or just the favorites? (2)  " CHOICE
 case "$CHOICE" in
 	1)
 		general_packages
-		pip_upgrade
+		pip2_upgrade
 		;;
 	2)
 		choice_packages
-		pip_upgrade
+		pip2_upgrade
 		;;
 	*)
 		echo "Please enter (1) or (2)"
 		;;
 esac
 
-if [[ pip_upgrade ]] ; then
+if [[ pip2_upgrade ]] ; then
     echo "Done."
     rm "$LIST"
 else
