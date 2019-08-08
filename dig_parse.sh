e6d4974689bee6cdf1aea4f7c7e5c66ebbb9b9af
bblinder <filmmaven@gmail.com>
2019-08-07 20:31:40 -0400

    Adding some placeholders for future improvements

--- dig_parse.sh
+++ dig_parse.sh
@@ -10,19 +10,30 @@
 # Sleep is not necessary, but always a nice option.
 
 sleep=0.2
+#TLD_list='/tmp/TLD_list.txt'
+
+#remove_akamai_domains(){
+#	grep -Eiv "akamaihd|edgesuite|edgekey"
+#}
+
+#strip_subdomains(){
+#  awk -F\. '{print $(NF-1) FS $NF}'
+#}
+
+#cat "$1" | strip_subdomains | remove_akamai_domains > "$TLD_list"
 
 # Don't read lines with 'for'. Use a 'while' loop and 'read'.
 while IFS= read -r domain
 do
   sleep "$sleep"
   echo "$domain"
-# Using +short to avoid auxiliary information.
-  ipaddress=$(dig $domain +short)
-  nameserver=$(dig ns "$domain" +short)
-# Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
-# Now with commas as delimiters, software can convert results to spreadsheet.
-  ipaddress_space=$(echo -e "$ipaddress" | tr '\n' ',' | tr -d "[:blank:]")
+  # Using +short to avoid auxiliary information.
+  ipaddress=(dig $domain +short)
+  nameserver=$(dig ns $domain +short)
+  # Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
+  # Now with commas as delimiters, software can convert results to spreadsheet.
+  #cname_space=$(echo -e "$cname" | tr '\n' ',' | tr -d "[:blank:]")
   nameserver_space=$(echo -e "$nameserver" | tr '\n' ',' | tr -d "[:blank:]")
-# Outputting to the filename output.csv
-  echo -e "$domain,$ipaddress_space$nameserver_space" >> output.csv
+  # Outputting to the filename domains.csv
+  echo -e "$domain,$nameserver_space" >> "$2".csv
 done < "$1"
