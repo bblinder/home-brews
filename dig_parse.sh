7bfa757c12c7ef87dbd62a7c84a1f7e3c4425cf6
bblinder <filmmaven@gmail.com>
2019-08-08 09:39:13 -0400

    some revisions for a later version

--- dig_parse.sh
+++ dig_parse.sh
@@ -10,30 +10,43 @@
 # Sleep is not necessary, but always a nice option.
 
 sleep=0.2
-#TLD_list='/tmp/TLD_list.txt'
 
-#remove_akamai_domains(){
-#	grep -Eiv "akamaihd|edgesuite|edgekey"
-#}
+# Saving this for a later iteration.
+<<EOF
+TLD_list='/tmp/TLD_list.txt'
 
-#strip_subdomains(){
-#  awk -F\. '{print $(NF-1) FS $NF}'
-#}
+remove_akamai_domains(){
+	grep -Eiv "akamaihd|edgesuite|edgekey"
+}
 
-#cat "$1" | strip_subdomains | remove_akamai_domains > "$TLD_list"
+strip_subdomains(){
+  awk -F\. '{print $(NF-1) FS $NF}'
+}
+
+cat "$1" | strip_subdomains | remove_akamai_domains > "$TLD_list"
+EOF
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]
+then
+    echo "No input files found."
+    exit 1
+fi
 
 # Don't read lines with 'for'. Use a 'while' loop and 'read'.
 while IFS= read -r domain
 do
   sleep "$sleep"
   echo "$domain"
+
   # Using +short to avoid auxiliary information.
   ipaddress=(dig $domain +short)
   nameserver=$(dig ns $domain +short)
+
   # Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
   # Now with commas as delimiters, software can convert results to spreadsheet.
   #cname_space=$(echo -e "$cname" | tr '\n' ',' | tr -d "[:blank:]")
   nameserver_space=$(echo -e "$nameserver" | tr '\n' ',' | tr -d "[:blank:]")
+
   # Outputting to the filename domains.csv
   echo -e "$domain,$nameserver_space" >> "$2".csv
 done < "$1"
