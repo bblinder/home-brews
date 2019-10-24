86f34fee952afe34406ff5c90b185b4c3413c8b1
bblinder <filmmaven@gmail.com>
2019-10-24 14:57:30 -0400

    Rewrote the tool

--- dig_parse.sh
+++ dig_parse.sh
@@ -23,16 +23,16 @@ do
   echo -e "$domain"
 
   # Using +short to avoid auxiliary information.
-  ipaddress="$(dig $domain +short)"
+  ipaddress=$(dig $domain +short)
   # stripping subdomains and getting NS info
-  TLD_domain="$(echo -e $domain | awk -F\. '{print $(NF-1) FS $NF}')"
-  nameserver="$(dig ns $TLD_domain +short)"
+  TLD_domain=$(echo -e $domain | awk -F\. '{print $(NF-1) FS $NF}')
+  nameserver=$(dig ns $TLD_domain +short)
 
   # Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
   # Now with commas as delimiters, software can convert results to spreadsheet.
   #cname_space=$(echo -e "$cname" | tr '\n' ',' | tr -d "[:blank:]")
-  nameserver_space="$(echo -e $nameserver | tr '\n' ',' | tr -d "[:blank:]")"
+  nameserver_space=$(echo -e "$nameserver" | tr '\n' ',' | tr -d "[:blank:]")
 
   # Outputting to the filename of choice for "$2"
-  echo -e $domain,$nameserver_space >> "$2"
+  echo -e "$domain,$nameserver_space" >> "$2"
 done < "$1"
