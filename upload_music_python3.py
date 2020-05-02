7265feeabf2a96b1b043821435333390cb007def
bblinder <filmmaven@gmail.com>
2020-05-02 16:03:51 -0400

    modified:   upload_music_python3.py

--- upload_music_python3.py
+++ upload_music_python3.py
@@ -11,14 +11,6 @@ except ImportError:
 	print ("Exiting...")
 	sys.exit()
 
-# A unique id as a MAC address, eg '00:11:22:33:AA:BB'. 
-# Per the docs, this should only be provided in cases where the default (host MAC address incremented by 1) will not work.
-mac_address = '38:F9:D3:57:68:FC'
-
-mm = Musicmanager()
-# mm.perform_oauth() # For the first run/implementation only. Not needed afterwards.
-mm.login(uploader_id=mac_address)
-
 ''' For later, when I'm generating a random MAC address
 def rand_mac():
     return "%02x:%02x:%02x:%02x:%02x:%02x" % (
@@ -30,6 +22,15 @@ def rand_mac():
         random.randint(0, 255)
         )
 '''
+
+# A unique id as a MAC address, eg '00:11:22:33:AA:BB'. 
+# Per the docs, this should only be provided in cases where the default (host MAC address incremented by 1) will not work.
+# It's recommended to use the same MAC address for continued uploads from the same machine.
+mac_address = '38:F9:D3:57:68:FC'
+
+mm = Musicmanager()
+# mm.perform_oauth() # For the first run/implementation only. Not needed afterwards.
+mm.login(uploader_id=mac_address)
 
 track = sys.argv[1]
 print(track)
