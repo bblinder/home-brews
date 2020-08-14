3bc7fe9f8976802aacf6477be38ad7d12d33f018
bblinder <filmmaven@gmail.com>
2020-08-14 11:47:48 -0400

    modified:   thesaurus.sh

--- thesaurus.sh
+++ thesaurus.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Not my work, just keeping it here as a handy reference. Needs lynx and html2text to work.
 
 BROWSER="/usr/local/bin/lynx -source"
 WEBSITE="http://thesaurus.com/browse/$1"
-HTML2TEXT="/usr/local/bin/html2text -style compact"
+HTML2TEXT="/usr/local/bin/html2text"
 
 if test "$1"; then
 	${BROWSER} ${WEBSITE} | ${HTML2TEXT} | ${PAGER}
