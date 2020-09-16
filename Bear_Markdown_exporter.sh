4e33a771f2e25e8b43268524440d9aa2403ce0f1
bblinder <filmmaven@gmail.com>
2020-09-16 14:51:32 -0400

    new file:   Bear_Markdown_exporter.sh

---
+++ Bear_Markdown_exporter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# uses https://github.com/andymatuschak/Bear-Markdown-Export and saves it
+# to my OneDrive folder.
+
+python3 ~/Github/Bear-Markdown-Export/bear_export_sync.py --out ~/OneDrive/Bear/Bear --backup ~/OneDrive/Bear/Backup
+
