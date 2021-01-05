f1edc599712c5ff7f98314cad3d7a076b033dc12
bblinder <filmmaven@gmail.com>
2021-01-04 22:05:07 -0500

    adding google DNS to dig

--- dig_parse.sh
+++ dig_parse.sh
@@ -19,20 +19,20 @@ fi
 
 sleep=0.1
 master_dig='/tmp/dig_domain.txt'
-ns_dig='/tmp/ns_dig.txt'
+#ns_dig='/tmp/ns_dig.txt'
 
 # Don't read lines with 'for'. Use a 'while' loop and 'read'.
 while IFS= read -r domain
 do
   sleep "$sleep"
   echo -e "$domain"
-  
+
   # Creating our cached dig output. Using +short to avoid auxiliary information.
-  dig "$domain" +short > /tmp/dig_domain.txt
+  dig @8.8.8.8 "$domain" +short > /tmp/dig_domain.txt
 
   cname="$(cat $master_dig | awk 'NR==1{print $1}')"
   ipaddress="$(cat $master_dig | awk '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}' | sed '/^$/d' | awk 'NR==1{print $1}')"
-  
+
   # stripping subdomains and getting nameserver data
   TLD_domain="$(echo -e $domain | sed "s/^[^.]*\.//")"
   nameserver="$(dig ns "$TLD_domain" +short | awk 'NR==1{print $1}')"
@@ -45,7 +45,7 @@ do
   #cname_space=$(echo -e "$cname" | tr '\n' ',' | tr -d "[:blank:]")
   nameserver_space="$(echo -e "$nameserver" | tr '\n' ' ' | tr -d "[:blank:]")"
   ipaddr_space="$(echo -e "$ipaddress" | tr '\n' ',' | tr -d "[:blank:]")"
-  
+
   # Outputting to the filename of choice for "$2" (ideally a csv)
   echo -e "$provider,$domain,$nameserver_space,$cname,$ipaddr_space" >> "$2"
 done < "$1"
