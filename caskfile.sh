2603d54715d6e1a8b4ce6f38c9ef89e1e20072e8
bblinder <filmmaven@gmail.com>
2021-01-06 11:24:59 -0500

    adding brew taps

--- caskfile.sh
+++ caskfile.sh
@@ -15,7 +15,7 @@ fi
 brew_array=(ccat neofetch ack axel curl cmus exiftool csshx cowsay htop icdiff\
 	irssi httpie lynx mplayer mtr osxfuse p7zip pstree sshfs tcpdump\
 	speedtest_cli pv rtmpdump tldr wget youtube-dl zsh git m-cli pwgen\
-	gnu-units lnav ranger watch iftop ffmpeg ssh-copy-id spoof-mac fzf\
+	gnu-units lnav ranger watch iftop ffmpeg ssh-copy-id spoof-mac fzf nextdns\
 	parallel pdfgrep dtrx lepton sslh jq woof coreutils goaccess exa wifi-password\
 	zsh-syntax-highlighting mediainfo pandoc rtv cheat figlet rig fortune httrack\
 	akamai bat prettyping magic-wormhole python3 heroku streamlink imagemagick aria2\
@@ -30,7 +30,7 @@ cask_array=(alfred firefox krisp nightowl docker lulu obsidian onedrive \
 	iterm2 xquartz caprine calibre ransomwhere microsoft-edge telegram espanso \
 	spotify beardedspice the-unarchiver gpgtools iina visual-studio-code \
 	numi handbrake carbon-copy-cloner jumpcut etrecheckpro rectangle typora \
-	charles visual-studio-code malwarebytes postman anki appcleaner nextdns\
+	charles visual-studio-code malwarebytes postman anki appcleaner \
 	oversight font-input grandperspective bunch signal android-platform-tools \
 	imageoptim deckset lastpass backblaze do-not-disturb reikey gas-mask appcleaner)
 
