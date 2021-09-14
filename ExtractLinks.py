b9b94a4502be81471be1f33d32d3232b4ffed5b5
bblinder <filmmaven@gmail.com>
2021-09-14 11:59:32 -0400

    testing argparse and gooey

--- ExtractLinks.py
+++ ExtractLinks.py
@@ -4,14 +4,26 @@
 
 import sys
 import requests
+import argparse
+#from gooey import Gooey, GooeyParser
 from bs4 import BeautifulSoup
 
+#@Gooey
+def extract_links():
+	#parser = GooeyParser(description="List the URLs on a page")
+	#parser.add_argument('url', help="The URL of the page we're extracting links from", type=str)
+	parser = argparse.ArgumentParser(description='List the URLs on a page')
+	parser.add_argument('url', help="The URL of the page we're extracting links from", type=str)
+	args = vars(parser.parse_args())
+	#print(args)
 
-url = sys.argv[1] #the url of the page we're extracting links from.
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
+	#url = input("URL of the page: ")
+	url = sys.argv[1] #the url of the page we're extracting links from.
+	reqs = requests.get(url)
+	soup = BeautifulSoup(reqs.text, 'html.parser')
 
-urls = []
-for link in soup.find_all('a'):
-	print(link.get('href'))
+	urls = []
+	for link in soup.find_all('a'):
+		print(link.get('href'))
 
+extract_links()
