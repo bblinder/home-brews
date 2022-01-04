ed9b3ae7e040cdf95d7f4d361611f5c104829885
bblinder <filmmaven@gmail.com>
2022-01-04 14:21:16 -0500

    modified:   battery_status.sh

--- battery_status.sh
+++ battery_status.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # A script to trigger a music file if battery level falls below 20%.
 # Mac OS X only
@@ -11,15 +11,18 @@ if [[ "$(uname -s)" != "Darwin" ]] ; then
     exit 1
 fi
 
+if [[ ! "$(command -v noti)" ]] ; then
+	echo "::: Noti not installed."
+	exit 1
+fi
+
 battery_level="$(pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f1 -d';' | sed -e 's/%//')"
 
-music_file=/path/to/file
+music_file="/Users/$USER/Music/Low_Battery.mp3"
 
 if [[ "$battery_level" -lt 20 ]] ; then
-	if [[ "$(command -v noti)" ]] ; then
-		noti -t "WARNING" -m "Battery level at $battery_level%"
-		afplay -t 30 "$music_file"
-	else
-		afplay -t 30 "$music_file"
-	fi
+	noti -t "WARNING" -m "Battery level at $battery_level%"
+	afplay -t 30 "$music_file"
+else
+	afplay -t 30 "$music_file"
 fi
