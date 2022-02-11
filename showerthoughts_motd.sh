d99396d64c8b09e3712fd26b96c0081cf2c0a80e
bblinder <filmmaven@gmail.com>
2022-02-10 22:32:15 -0500

    modified:   showerthoughts_motd.sh

--- showerthoughts_motd.sh
+++ showerthoughts_motd.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -euo pipefail
+set -Eeuo pipefail
 
 # Stolen and modifed from a reddit thread.
 
-username="" # your reddit handle
+username=$1 # your reddit handle
 
 showerthoughts=$(http 'https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=week&limit=100' | \
 python2.7 -c 'import sys, random, json; randnum = random.randint(0,99); response = json.load(sys.stdin)["data"]["children"][randnum]["data"]; print "\n\"" + response["title"] + "\""; print "    -" + response["author"] + "\n";')
 
 echo $showerthoughts | cowsay | lolcat
+exit 0
