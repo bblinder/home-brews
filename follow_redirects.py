abe2e4522fa6ae38d31097b0357ede80a39a0913
bblinder <filmmaven@gmail.com>
2022-03-12 13:03:43 -0500

    new file:   follow_redirects.py

---
+++ follow_redirects.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+try: 
+    import pyperclip as pc
+except ImportError:
+    print("::: Pyperclip module not found.")
+
+def get_url(url):
+    r = requests.get(url, allow_redirects=True)
+    return r.url
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Follows URL redirects and returns a final address")
+    parser.add_argument("URL", help="the URL to follow")
+    args = parser.parse_args()
+    url = args.URL
+
+    # Copies the output to the system clipboard.
+
+    output = get_url(url)
+    print(output)
+    pc.copy(output)
