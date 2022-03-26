921597a78481a54e1e333a3d147c9ded470fcbdc
bblinder <filmmaven@gmail.com>
2022-03-26 16:45:43 -0400

    modified:   ExtractLinks.py

--- ExtractLinks.py
+++ ExtractLinks.py
@@ -5,20 +5,15 @@
 import sys
 import requests
 import argparse
-#from gooey import Gooey, GooeyParser
 from bs4 import BeautifulSoup
 
 #@Gooey
 def extract_links():
-	#parser = GooeyParser(description="List the URLs on a page")
-	#parser.add_argument('url', help="The URL of the page we're extracting links from", type=str)
 	parser = argparse.ArgumentParser(description='List the URLs on a page')
 	parser.add_argument('url', help="The URL of the page we're extracting links from", type=str)
 	args = vars(parser.parse_args())
-	#print(args)
 
-	#url = input("URL of the page: ")
-	url = sys.argv[1] #the url of the page we're extracting links from.
+	url = args['url']
 	reqs = requests.get(url)
 	soup = BeautifulSoup(reqs.text, 'html.parser')
 
