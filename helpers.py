8520d6fadd142d84233f3abf7ae6d9e8e4d8b17a
bblinder <filmmaven@gmail.com>
2022-03-28 14:00:22 -0400

    new file:   helpers.py

---
+++ helpers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Lambda function to "Clear" the terminal
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+clear()
+
+# Convert bytes depending on file size
+def convert_bytes(bytes_number):
+    tags = [ "Bytes", "KB", "MB", "GB", "TB" ]
+  
+    i = 0
+    double_bytes = bytes_number
+  
+    while (i < len(tags) and  bytes_number >= 1024):
+            double_bytes = bytes_number / 1024.0
+            i = i + 1
+            bytes_number = bytes_number / 1024
+  
+    return str(round(double_bytes, 2)) + " " + tags[i]
+
