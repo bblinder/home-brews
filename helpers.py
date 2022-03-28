c363d10856301b1119f9b2e12da226dcea1f369e
bblinder <filmmaven@gmail.com>
2022-03-28 14:35:11 -0400

    modified:   helpers.py

--- helpers.py
+++ helpers.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 # Lambda function to "Clear" the terminal
+'''
 clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 clear()
+'''
 
 # Convert bytes depending on file size
 def convert_bytes(bytes_number):
@@ -18,3 +20,10 @@ def convert_bytes(bytes_number):
   
     return str(round(double_bytes, 2)) + " " + tags[i]
 
+def check_valid_url(url):
+    try:
+        import validators
+    except ImportError:
+        print("Please install validators module")
+        return False
+    return validators.url(url)
