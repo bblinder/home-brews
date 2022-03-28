b25565adaafbc336a8a1d4adad27536a25c036dc
bblinder <filmmaven@gmail.com>
2022-03-27 22:31:25 -0400

    modified:   smmry.py

--- smmry.py
+++ smmry.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 import requests
-import sys
 import urllib.parse
+import argparse
+import json
+import os
 
-API_key = '[API Key]'
-article_URL_raw = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', help='URL to summarize', required=True)
+parser.add_argument('-k', '--key', help='API key', required=False)
+args = parser.parse_args()
+
+if os.path.isfile('.env'): # Checking if .env file exists
+    from dotenv import load_dotenv
+    load_dotenv() # importing .env file as a environment variable(s)
+    API_key = os.environ['SMMRY_API_KEY']
+else:
+    API_key = args.key
+
+article_URL_raw = args.url
 article_URL_formatted = urllib.parse.quote_plus(article_URL_raw)
 url = "https://api.smmry.com?SM_API_KEY=" + API_key + "&SM_LENGTH=10&SM_URL=" + article_URL_formatted
 
@@ -14,6 +27,11 @@ headers= {}
 
 response = requests.request("POST", url, headers=headers, data = payload)
 
-#json_data = json.loads(response.text.encode('utf8'))
-print(response.text.encode('utf8'))
-
+if __name__ == '__main__':
+    if not API_key:
+        print("Please provide an API key")
+        exit(1)
+    else:
+        #print(response.text.encode('utf8'))
+        json_data = json.loads(response.text.encode('utf8'))
+        print(json_data['sm_api_content'])
