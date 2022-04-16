5376cf0884aa432d47759c9f580200b2662ed9a8
bblinder <filmmaven@gmail.com>
2022-04-16 14:14:13 -0400

    modified:   threader_app.py

--- threader_app.py
+++ threader_app.py
@@ -11,23 +11,32 @@ parser = argparse.ArgumentParser(description='Get the thread ID from a twitter U
 parser.add_argument('url', help='the URL of the thread')
 args = parser.parse_args()
 
-url = args.url
 valid_twitter_domains = ['twitter.com', 'mobile.twitter.com', 't.co']
 
-def format_url(url):
-    # remove www. from the URL
-    if 'www.' in url:
-        url = url.replace('www.', '')
-    
-    # add https:// if it's missing
-    if not url.startswith('https://') and not url.startswith('http://'):
-        url = 'https://' + url
-    return url
 
-url = format_url(url)
+def format_url():
+    twitter_url = args.url
+    # remove www. from the URL
+    if 'www.' in twitter_url:
+        twitter_url = twitter_url.replace('www.', '')
+
+    # add https:// if it's missing
+    if not twitter_url.startswith('https://') and not twitter_url.startswith('http://'):
+        twitter_url = 'https://' + twitter_url
+    return twitter_url
+
+
+url = format_url()
+
 
 # Make a HEAD request to the URL to check if valid/still exists
-check_url = lambda url: requests.head(url).status_code == 200
+def check_url(url):
+    try:
+        response = requests.head(url)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
 
 # get the thread ID from a twitter URL
 def get_thread_id(url):
@@ -42,14 +51,14 @@ def get_thread_id(url):
         print('Invalid URL')
         sys.exit(1)
 
-thread_id = get_thread_id(url)
 
 # Generating a "The Thread App" URL
-def threader_app(thread_id):
+def threader_app():
+    thread_id = get_thread_id(url)
     threader_domain = "https://threadreaderapp.com/thread/"
     threader_url = threader_domain + thread_id + ".html"
     print(threader_url)
 
 
 if __name__ == '__main__':
-    threader_app(thread_id)
+    threader_app()
