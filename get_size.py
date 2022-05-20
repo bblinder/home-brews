ec483f14608a2be647de3641dbe78ea0f316fede
bblinder <filmmaven@gmail.com>
2022-05-20 12:13:19 -0400

    modified:   get_size.py

--- get_size.py
+++ get_size.py
@@ -8,11 +8,13 @@ def get_filesize(file):
     return os.path.getsize(file)
 
 
-def get_dir_size(path):
+def get_dir_size(path='.'):
     total_size = 0
     with os.scandir(path) as it:
         for entry in it:
-            if entry.is_file():
+            if entry.is_file() and entry.name.startswith('.'):
+                total_size += entry.stat().st_size
+            elif entry.is_file():
                 total_size += entry.stat().st_size
             elif entry.is_dir():
                 total_size += get_dir_size(entry.path)
