3f2432708c106e687e4d592eb0362c1de629c104
bblinder <filmmaven@gmail.com>
2022-09-22 12:54:44 -0400

    modified:   ExtractLinks.py 	modified:   zip_code_lookup.py

--- ExtractLinks.py
+++ ExtractLinks.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
-#simple script to extract links from a web page.
+# simple script to extract links from a web page.
 
-import sys
-import requests
 import argparse
+
+import requests
 from bs4 import BeautifulSoup
 
-#@Gooey
+
+# @Gooey
 def extract_links():
-	parser = argparse.ArgumentParser(description='List the URLs on a page')
-	parser.add_argument('url', help="The URL of the page we're extracting links from", type=str)
-	args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser(description="List the URLs on a page")
+    parser.add_argument(
+        "url", help="The URL of the page we're extracting links from", type=str
+    )
+    args = vars(parser.parse_args())
 
-	url = args['url']
-	reqs = requests.get(url)
-	soup = BeautifulSoup(reqs.text, 'html.parser')
+    url = args["url"]
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, "html.parser")
 
-	urls = []
-	for link in soup.find_all('a'):
-		print(link.get('href'))
+    urls = []
+    for link in soup.find_all("a"):
+        print(link.get("href"))
+
 
 if __name__ == "__main__":
-	extract_links()
+    extract_links()
--- zip_code_lookup.py
+++ zip_code_lookup.py
@@ -8,9 +8,9 @@ import sys
 
 import yaml
 
-args = argparse.ArgumentParser(description="Query SmartyStreets API")
-args.add_argument("file_name", help="CSV file with zip codes")
-args.parse_args()
+argparser = argparse.ArgumentParser(description="Query SmartyStreets API")
+argparser.add_argument("file_name", help="CSV file with zip codes")
+args = argparser.parse_args()
 
 with open("config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
@@ -34,7 +34,7 @@ def zip_request():
             value_from_csv = row[0]
             # headers = {"content-type": "application/json"}
             url = f"https://us-zipcode.api.smartystreets.com/lookup?auth-id={auth_id}&auth-token={auth_token}&zipcode={value_from_csv}"
-            r = requests.get(url, timeout=5)
+            r = requests.get(url, timeout=10)
             print(r.json())
 
 
