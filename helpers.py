52742440c2995d194534751ada4ec1cde06d969f
bblinder <filmmaven@gmail.com>
2022-10-01 11:32:49 -0400

    modified:   helpers.py 	modified:   thesaurus.py

--- helpers.py
+++ helpers.py
@@ -1,16 +1,14 @@
 import os
 
 # Lambda function to "Clear" the terminal
-clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+clear = lambda: os.system("cls" if os.name == "nt" else "clear")
 
-
-# clear()
 
 def get_downloads_folder():
-    if os.name == 'nt':
-        return os.path.join(os.environ['USERPROFILE'], 'Downloads')
+    if os.name == "nt":
+        return os.path.join(os.environ["USERPROFILE"], "Downloads")
     else:
-        return os.path.join(os.path.expanduser('~'), 'Downloads')
+        return os.path.join(os.path.expanduser("~"), "Downloads")
 
 
 # Convert bytes depending on file size
@@ -39,11 +37,26 @@ def check_valid_url(url):
 
 def deal_with_it():
     from time import sleep
-    
-    print('( •_•) ')
+
+    print("( •_•) ")
     sleep(0.75)
-    print('\r( •_•)>⌐■-■')
-    sleep(.75)
-    print('\r               ')
-    print('\r(⌐■_■)')
+    print("\r( •_•)>⌐■-■")
+    sleep(0.75)
+    print("\r               ")
+    print("\r(⌐■_■)")
     sleep(0.5)
+
+
+# Generate random MAC address
+def generate_mac():
+    import random
+
+    mac = [
+        0x00,
+        0x16,
+        0x3E,
+        random.randint(0x00, 0x7F),
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
+    ]
+    return ":".join(map(lambda x: "%02x" % x, mac))
--- thesaurus.py
+++ thesaurus.py
@@ -10,6 +10,8 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.table import Table
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -42,11 +44,52 @@ def get_word(endpoint, word):
     return results
 
 
+# def main():
+#     endpoint = args.endpoint
+#     word = args.word.lower()
+#     results = get_word(endpoint, word)
+#     print(json.dumps(json.loads(results), indent=4, sort_keys=True))
+
+
 def main():
     endpoint = args.endpoint
     word = args.word.lower()
     results = get_word(endpoint, word)
-    print(json.dumps(json.loads(results), indent=4, sort_keys=True))
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+
+    if endpoint == "entries":
+        table.add_column("Word", style="dim", width=12)
+        table.add_column("Definition", style="dim", width=12)
+        table.add_column("Example", style="dim", width=12)
+        table.add_column("Synonyms", style="dim", width=12)
+
+        for result in json.loads(results)["results"]:
+            for lexical_entry in result["lexicalEntries"]:
+                for entry in lexical_entry["entries"]:
+                    for sense in entry["senses"]:
+                        table.add_row(
+                            result["word"],
+                            sense["definitions"][0],
+                            sense["examples"][0]["text"],
+                            # return first 15 synonyms
+                            ", ".join(
+                                synonym["text"] for synonym in sense["synonyms"][:15]
+                            ),
+                        )
+
+    elif endpoint == "thesaurus":
+        table.add_column("Word", style="dim", width=12)
+        table.add_column("Synonym", style="dim", width=12)
+
+        for result in json.loads(results)["results"]:
+            for lexical_entry in result["lexicalEntries"]:
+                for entry in lexical_entry["entries"]:
+                    for sense in entry["senses"]:
+                        for synonym in sense["synonyms"]:
+                            table.add_row(result["word"], synonym["text"])
+
+    console.print(table)
 
 
 if __name__ == "__main__":
