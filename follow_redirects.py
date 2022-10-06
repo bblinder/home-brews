8c486f445d66fb31ab7207be3eb5feb084c2c87d
bblinder <filmmaven@gmail.com>
2022-10-06 11:12:35 -0400

    modified:   follow_redirects.py

--- follow_redirects.py
+++ follow_redirects.py
@@ -1,33 +1,52 @@
 #!/usr/bin/env python3
 
-import requests
+import sys
 
-try: 
+import requests
+import validators
+
+try:
     import pyperclip as pc
 except ImportError:
     print("::: Pyperclip module not found.")
-    exit(1)
+    sys.exit(1)
+
+
+def validate_url(url):
+    if not validators.url(url):
+        print(f"::: '{url}' is not a valid URL.")
+        sys.exit(1)
+
 
 # Follow URL redirect, print any hops along the way
 def follow_redirect():
     # By default, whatever's currently in the clipboard gets pasted.
     if args.url:
         input = args.url
+        validate_url(input)
     else:
         input = pc.paste()
-    r = requests.get(input, allow_redirects=True)
-    
+        validate_url(input)
+    r = requests.get(input, allow_redirects=True, timeout=5)
+
     if r.history:
         print(r.history)
     return r.url
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="Follows URL redirects and returns a final address")
+
+    parser = argparse.ArgumentParser(
+        description="Follows URL redirects and returns a final address"
+    )
     parser.add_argument(
-        "-u", "--url", 
-        help="the URL to follow. By default, whatever's currently in the clipboard gets pasted. Use '-u' to specify a URL manually.",
-        required=False)
+        "-u",
+        "--url",
+        help="the URL to follow. By default, whatever's currently in the clipboard gets pasted.\
+            Use '-u' to specify a URL manually.",
+        required=False,
+    )
     args = parser.parse_args()
     url = args.url
 
