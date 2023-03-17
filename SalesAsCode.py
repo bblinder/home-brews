3b33f8edba0d40b7b5e741721ec7d9fa657b1495
bblinder <filmmaven@gmail.com>
2023-03-17 17:58:26 -0400

    modified:   SalesAsCode.py

--- SalesAsCode.py
+++ SalesAsCode.py
@@ -9,16 +9,18 @@ import logging
 import os
 import random
 import sys
-import time
 
 import gspread
+from cachetools import TTLCache
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
 
 logging.basicConfig(level=logging.INFO)
 
+cache = TTLCache(maxsize=1, ttl=3600)
 
-def authenticate(args):
+
+def authenticate():
     """Authenticate with Google Sheets API and return the gsheet object."""
     if args.credentials:
         creds = args.credentials
@@ -46,39 +48,39 @@ def authenticate(args):
     return gsheet
 
 
-def get_sales_terms(args):
-    """Get sales terms from Google Sheets, cache them locally, and pick one at random."""
-    gsheet = authenticate(args)
-    temp_directory = os.path.join(os.path.expanduser("~"), ".cache", "salesascode")
-    temp_file = os.path.join(temp_directory, "sales_terms.txt")
-
-    if not os.path.exists(temp_directory):
-        os.makedirs(temp_directory)
-
+def fetch_sales_terms(gsheet):
+    """Fetch sales terms from Google Sheets."""
     try:
-        if os.path.exists(temp_file):
-            # check the timestamp of the file
-            file_timestamp = os.path.getmtime(temp_file)
-            current_timestamp = time.time()
-            if current_timestamp - file_timestamp > 3600:
-                # the file is older than 1 hour, re-fetch the sales terms
-                sales_terms = gsheet.col_values(1)[1:]
-                with open(temp_file, "w", encoding="utf-8") as f:
-                    f.write("\n".join(sales_terms))
-            else:
-                # the file is younger than 1 hour, use the cached sales terms
-                with open(temp_file, "r", encoding="utf-8") as f:
-                    sales_terms = f.read().splitlines()
-        else:
-            # the file doesn't exist, fetch the sales terms and cache them
-            sales_terms = gsheet.col_values(1)[1:]
-            with open(temp_file, "w", encoding="utf-8") as f:
-                f.write("\n".join(sales_terms))
+        sales_terms = gsheet.col_values(1)[1:]
+        return sales_terms
+    except Exception as err:
+        logging.error("Failed to fetch sales terms: %s", err)
+        return None
 
-        return random.choice(sales_terms)
-    except Exception as e:
-        logging.error(e)
-        sys.exit(1)
+
+def cache_sales_terms(sales_terms):
+    """Cache sales terms locally."""
+    try:
+        cache["sales_terms"] = sales_terms
+    except Exception as err:
+        logging.error("Failed to cache sales terms: %s", err)
+
+
+def get_sales_terms():
+    """Get sales terms from Google Sheets, cache them locally, and pick one at random."""
+    cached_terms = cache.get("sales_terms")
+    if cached_terms:
+        sales_terms = cached_terms
+    else:
+        gsheet = authenticate()
+        if not gsheet:
+            logging.error("Failed to authenticate with Google Sheets. Exiting...")
+            return None
+        sales_terms = fetch_sales_terms(gsheet)
+        if not sales_terms:
+            return None
+        cache_sales_terms(sales_terms)
+    return random.choice(sales_terms)
 
 
 def spongecase(term):
@@ -88,21 +90,20 @@ def spongecase(term):
     )
 
 
-def ask_if_rep_talking(args):
+def ask_if_rep_talking():
     """A while loop asking if the rep is still talking."""
     while True:
         try:
             answer = input("Is the rep still talking? (y/n) ").lower()
             if answer != "y":
                 break
-            sales_term = get_sales_terms(args)
+            sales_term = get_sales_terms()
             if args.spongecase:
                 print(spongecase(sales_term))
             else:
                 print(f"{sales_term}")
-        except Exception as e:
-            logging.error(e)
-            sys.exit(1)
+        except Exception as err:
+            logging.error("Failed to get sales term: %s", err)
 
 
 if __name__ == "__main__":
@@ -122,4 +123,8 @@ if __name__ == "__main__":
     )
     args = argparser.parse_args()
 
-    ask_if_rep_talking(args)
+    if not authenticate():
+        logging.error("Failed to authenticate with Google Sheets. Exiting...")
+        sys.exit(1)
+
+    ask_if_rep_talking()
