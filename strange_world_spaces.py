e3f7a5e162490f2e30c1fe719b570a040bf69ea3
bblinder <filmmaven@gmail.com>
2023-04-03 09:41:38 -0400

    new file:   strange_world_spaces.py

---
+++ strange_world_spaces.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+"""
+CLI utility to convert text to uppercase and/or add spaces, a la "Strange World".
+
+Accepts input text from stdin or as a positional argument.
+
+Usage:
+    your_script.py [-u] [-s] [text]
+
+Arguments:
+    text            The text to be processed. If not provided, the script reads from stdin.
+    -u, --uppercase Convert the text to uppercase.
+    -s, --spaces    Add spaces between characters in the text.
+
+Example:
+    echo "example text" | ./your_script.py -u -s
+    Output: "E X A M P L E   T E X T"
+"""
+
+
+import argparse
+import sys
+
+
+def add_spaces(text):
+    """Add spaces between characters in a string."""
+    spaced_text = " ".join(text)
+    return spaced_text
+
+
+def convert_to_uppercase(text):
+    """Convert a string to uppercase."""
+    upper_text = text.upper()
+    return upper_text
+
+
+def main():
+    """Deciding whether to use stdin or a positional argument,
+    and then passing the text to the appropriate functions."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text", help="text to convert", nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "-u", "--uppercase", help="convert to uppercase", action="store_true"
+    )
+    parser.add_argument("-s", "--spaces", help="add spaces", action="store_true")
+    args = parser.parse_args()
+
+    if args.text == sys.stdin:
+        text = sys.stdin.read().strip()
+    else:
+        text = args.text
+
+    if not args.uppercase and not args.spaces:
+        text = add_spaces(text)
+
+    if args.uppercase:
+        text = convert_to_uppercase(text)
+
+    if args.spaces:
+        text = add_spaces(text)
+
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
