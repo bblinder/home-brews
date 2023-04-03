ef47ad9859bc3afda83e29fcf9e19587bae8f7cd
bblinder <filmmaven@gmail.com>
2023-04-03 14:33:04 -0400

    modified:   scanPDF.py

--- LookScanned/scanPDF.py
+++ LookScanned/scanPDF.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env python3
 
 """
-A pythonic wrapper around imagemagick and ghostscript to make a PDF 'look scanned', and crappily at that.
+A pythonic wrapper around imagemagick and ghostscript to make
+a PDF 'look scanned', and crappily at that.
+
+Example:
+    python3 scanPDF.py -o ~/Downloads/SCANNED.pdf ~/Documents/original_PDF.pdf
+
+By default, the output file will be saved to the same directory
+as the input file (unless specified otherwise with the -o flag)
 """
 
 import os
 import subprocess
 import sys
+from pathlib import Path
 from shutil import which
+from typing import Optional
 
 from halo import Halo
 
 cmds = {"convert": "ImageMagick", "gs": "GhostScript"}
 
-# iterate with .items()
 for cmd, package in cmds.items():
     if which(cmd) is None:
         print(f"Please install {package} first")
         sys.exit(1)
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
+script_dir = Path(__file__).resolve().parent
 
 
-def get_downloads_folder():
+def get_downloads_folder() -> Path:
     """
     Returns the path to the user's Downloads folder
     """
     if os.name == "nt":
-        return os.path.join(os.environ["USERPROFILE"], "Downloads")
-    else:
-        return os.path.join(os.path.expanduser("~"), "Downloads")
+        return Path(os.environ["USERPROFILE"]) / "Downloads"
+    return Path.home() / "Downloads"
 
 
-def imagemagick_convert(pdf):
+def imagemagick_convert(pdf: Path) -> Path:
     """
     Uses ImageMagick to convert the PDF to a crappy looking scanned PDF
     """
+    temp_file = script_dir / "TEMP.pdf"
     imagemagick_commands = [
         "convert",
         "-density",
         "150",
-        args.pdf,
+        str(pdf),
         "-colorspace",
         "gray",
         "-linear-stretch",
@@ -53,17 +61,14 @@ def imagemagick_convert(pdf):
         "Gaussian",
         "-rotate",
         "0.5",
-        "TEMP.pdf",
+        str(temp_file),
     ]
 
-    temp_file = os.path.join(script_dir, "TEMP.pdf")
-
     subprocess.run(imagemagick_commands, cwd=script_dir, check=True)
-    # return the pdf
     return temp_file
 
 
-def ghostscript_convert(imagemagick_output_pdf):
+def ghostscript_convert(imagemagick_output_pdf: Path, output: Path):
     """
     Uses GhostScript to manage size and put on the finishing touches"
     """
@@ -74,37 +79,37 @@ def ghostscript_convert(imagemagick_output_pdf):
         "-dNOPAUSE",
         "-dNOCACHE",
         "-sDEVICE=pdfwrite",
-        "-sOutputFile=" + args.output,
+        "-sOutputFile=" + str(output),
         "-sColorConversionStrategy=LeaveColorUnchanged",
         "-dAutoFilterColorImages=true",
         "-dAutoFilterGrayImages=true",
         "-dDownsampleMonoImages=true",
         "-dDownsampleGrayImages=true",
         "-dDownsampleColorImages=true",
-        imagemagick_output_pdf,
+        str(imagemagick_output_pdf),
     ]
 
-    if subprocess.run(ghostscript_commands, cwd=script_dir, check=True):
-        os.remove(imagemagick_output_pdf)
+    try:
+        subprocess.run(ghostscript_commands, cwd=script_dir, check=True)
+    finally:
+        imagemagick_output_pdf.unlink()
 
 
 @Halo(text="Scanning PDF... please wait... ", spinner="dots")
-def main():
+def main(pdf: Path, output: Optional[Path] = None):
     """
     Main function.
     """
-    output_file = os.path.splitext(args.pdf)[0] + "_SCANNED.pdf"
+    output_file = pdf.with_stem(pdf.stem + "_SCANNED")
 
-    # if no output arg is given, ensure it has the same name as the input file
-    if args.output is None:
-        args.output = os.path.join(get_downloads_folder(), output_file)
+    if output is None:
+        output = get_downloads_folder() / output_file
 
-    # if the output arg is given, ensure it has the correct extension
-    if os.path.splitext(args.output)[1] != ".pdf":
-        args.output = args.output + ".pdf"
+    if output.suffix != ".pdf":
+        output = output.with_suffix(".pdf")
 
-    im_pdf = imagemagick_convert(args.pdf)
-    ghostscript_convert(im_pdf)
+    im_pdf = imagemagick_convert(pdf)
+    ghostscript_convert(im_pdf, output)
 
 
 if __name__ == "__main__":
@@ -113,12 +118,12 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
         description="Make your PDF look manually scanned"
     )
-    argparser.add_argument("pdf", help="PDF file to scan")
+    argparser.add_argument("pdf", type=Path, help="PDF file to scan")
     argparser.add_argument(
         "-o",
         "--output",
+        type=Path,
         help="Output location + file name (optional)",
     )
     args = argparser.parse_args()
-
-    main()
+    main(args.pdf, args.output)
