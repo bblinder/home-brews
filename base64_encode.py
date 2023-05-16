991ceff06ab768b189b6e9f835f3d24ee0664263
bblinder <filmmaven@gmail.com>
2023-05-15 20:15:50 -0400

    modified:   base64_encode.py

--- base64_encode.py
+++ base64_encode.py
@@ -9,9 +9,13 @@ Usage:
 import argparse
 import base64
 import sys
+import tempfile
+from urllib.parse import urlparse
+
+import requests
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("image", nargs="?", help="path to image file")
+parser.add_argument("image", nargs="?", help="URL, or path to image file")
 parser.add_argument("--html", action="store_true", help="output HTML")
 parser.add_argument("--markdown", action="store_true", help="output Markdown")
 parser.add_argument("--json", action="store_true", help="output JSON")
@@ -26,9 +30,34 @@ image_formats = {
 }
 
 
+# if the image is a URL, validate and download it to a temporary file
+def download_image(image):
+    """Download the image to a temporary file."""
+    if urlparse(image).scheme in ["http", "https"]:
+        response = requests.get(image, stream=True, timeout=5)
+        response.raise_for_status()
+
+        # create a temporary file and write the image to the temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  # filter out keep-alive new chunks
+                    temp_file.write(chunk)
+            temp_file_path = temp_file.name  # get the path of the temporary file
+
+        # extract image format from the URL
+        image_format = get_image_format(image)
+
+        return temp_file_path, image_format
+
+    # if not an image return the original path and the format
+    return image, get_image_format(image)
+
+
 def get_image_format(path):
     """Get the image format from the file extension."""
-    return path.split(".")[-1].lower()
+    path = urlparse(path).path  # extract the path from the URL
+    filename = path.split("/")[-1]  # get the filename from the URL path
+    return filename.split(".")[-1].lower()
 
 
 def get_image_data_url(image_data, mime_type):
@@ -57,10 +86,19 @@ def output_json(img_element):
 def main():
     """Read from a file or stdin and output the encoded string."""
     if args.image:
-        with open(args.image, "rb") as image:
-            image_data = image.read()
-            image_format = get_image_format(args.image)
-            mime_type = image_formats[image_format]
+        try:
+            image_path, image_format = download_image(args.image)
+            with open(image_path, "rb") as image:
+                image_data = image.read()
+                mime_type = image_formats[image_format]
+        except FileNotFoundError:
+            print(f"Error: The file '{args.image}' was not found.")
+            return
+        except PermissionError:
+            print(
+                f"Error: Permission denied when trying to open the file '{args.image}'."
+            )
+            return
     else:
         image_data = sys.stdin.buffer.read()
         mime_type = "image/png"
