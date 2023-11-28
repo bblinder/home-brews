9c87d81e529b428fe1d1cb65d7312dd671b6c8a3
bblinder <filmmaven@gmail.com>
2023-11-28 16:00:54 -0500

    updating to PEP8 standards

--- follow_redirects.py
+++ follow_redirects.py
@@ -1,56 +1,123 @@
 #!/usr/bin/env python3
+"""
+A Python script to follow URL redirects and return the final address.
+Optionally interacts with the system clipboard to read/write URLs.
+"""
 
 import sys
-
-import requests
+import logging
+import argparse
 import validators
+import requests
 
 try:
     import pyperclip as pc
+
+    CLIPBOARD_ENABLED = True
 except ImportError:
-    print("::: Pyperclip module not found.")
-    sys.exit(1)
+    CLIPBOARD_ENABLED = False
+    logging.warning("::: Pyperclip module not found. Clipboard functionality disabled.")
 
 
 def validate_url(url):
+    """
+    Validates the given URL.
+
+    Args:
+        url (str): The URL to validate.
+
+    Raises:
+        SystemExit: If the URL is invalid.
+    """
     if not validators.url(url):
-        print(f"::: '{url}' is not a valid URL.")
+        logging.error(f"::: '{url}' is not a valid URL.")
         sys.exit(1)
 
 
-# Follow URL redirect, print any hops along the way
-def follow_redirect():
-    # By default, whatever's currently in the clipboard gets pasted.
-    if args.url:
-        input = args.url
-        validate_url(input)
-    else:
-        input = pc.paste()
-        validate_url(input)
-    r = requests.get(input, allow_redirects=True, timeout=5)
+def follow_redirect(input_url, timeout):
+    """
+    Follows the redirect of a given URL and returns the final URL.
 
-    if r.history:
-        print(r.history)
-    return r.url
+    Args:
+        input_url (str): The URL to follow.
+        timeout (int): Timeout value in seconds for the request.
+
+    Returns:
+        str: The final URL after following redirects.
+
+    Raises:
+        SystemExit: If an error occurs during the request.
+    """
+    validate_url(input_url)
+    try:
+        response = requests.get(input_url, allow_redirects=True, timeout=timeout)
+        if response.history:
+            logging.info(response.history)
+        return response.url
+    except requests.RequestException as e:
+        logging.error(f"::: Error during request: {e}")
+        sys.exit(1)
+
+
+def main(url, timeout):
+    """
+    Main function to execute the script logic.
+
+    Args:
+        url (str): The URL to process.
+        timeout (int): Timeout value in seconds.
+
+    Raises:
+        SystemExit: If no valid URL is provided or an error occurs.
+    """
+    final_url = ""
+    if url:
+        final_url = follow_redirect(url, timeout)
+    elif CLIPBOARD_ENABLED:
+        clipboard_content = pc.paste()
+        if validators.url(clipboard_content):
+            final_url = follow_redirect(clipboard_content, timeout)
+        else:
+            logging.error("::: Clipboard does not contain a valid URL.")
+            sys.exit(1)
+    else:
+        logging.error("::: No URL provided and clipboard functionality is disabled.")
+        sys.exit(1)
+
+    print(final_url)
+    if CLIPBOARD_ENABLED:
+        pc.copy(final_url)
 
 
 if __name__ == "__main__":
-    import argparse
+    logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(
-        description="Follows URL redirects and returns a final address"
+    PARSER = argparse.ArgumentParser(
+        description="Follows URL redirects and returns the final address"
     )
-    parser.add_argument(
+    PARSER.add_argument(
         "-u",
         "--url",
-        help="the URL to follow. By default, whatever's currently in the clipboard gets pasted.\
-            Use '-u' to specify a URL manually.",
+        help="Specify a URL manually. If not provided, the script will try to use the URL from the clipboard.",
         required=False,
     )
-    args = parser.parse_args()
-    url = args.url
+    PARSER.add_argument(
+        "-t",
+        "--timeout",
+        help="Set a custom timeout value in seconds (default is 5 seconds).",
+        type=int,
+        default=5,
+    )
+    PARSER.add_argument(
+        "--no-clipboard", action="store_true", help="Disable clipboard interaction."
+    )
+    ARGS = PARSER.parse_args()
 
-    # Copies the output to the system clipboard.
-    output = follow_redirect()
-    print(output)
-    pc.copy(output)
+    if ARGS.no_clipboard:
+        CLIPBOARD_ENABLED = False
+
+    if not ARGS.url and not CLIPBOARD_ENABLED:
+        PARSER.print_help()
+        sys.exit(1)
+
+    main(ARGS.url, ARGS.timeout)
