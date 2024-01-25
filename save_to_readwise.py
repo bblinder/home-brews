40b581149b252bcdaed20091f75ff74bd979c607
bblinder <bblinderman@splunk.com>
2024-01-25 11:56:49 -0500

    modified:   save_to_readwise.py

--- save_to_readwise.py
+++ save_to_readwise.py
@@ -18,6 +18,14 @@ args.add_argument("--tags", nargs="*", help="Tags to add to the article")
 args = args.parse_args()
 
 
+def check_env_vars():
+    """Check if the environment variables are set."""
+    if not READWISE_TOKEN:
+        raise Exception("READWISE_TOKEN environment variable not set.")
+    if not API_ENDPOINT:
+        raise Exception("API_ENDPOINT environment variable not set.")
+    return True
+    
 def add_article(url, tags):
     """Add an article to Readwise.
 
@@ -36,12 +44,14 @@ def add_article(url, tags):
 
     if response.status_code in {200, 201}:
         print(f"Added {url} to Readwise.")
+        print(f"Readwise URL: {response.json()['url']}")
     else:
         print(f"Failed to add {url}. Status code: {response.status_code}")
 
 
 def main():
     """Constructing the request and adding tags"""
+    check_env_vars()
     add_article(args.URL, args.tags)
 
 
