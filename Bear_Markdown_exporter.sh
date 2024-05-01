e4f5071e16fb84d02c9d20b8b71a22e7ab648705
bblinder-splunk <bblinderman@splunk.com>
2024-05-01 19:56:09 -0400

    Adding some checks

--- Bear_Markdown_exporter.sh
+++ Bear_Markdown_exporter.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
 # uses https://github.com/andymatuschak/Bear-Markdown-Export and saves it
-# to my OneDrive folder.
+# to my OneDrive folder. This script is run by a cron job every day.
 
-python3 ~/Github/Bear-Markdown-Export/bear_export_sync.py --out ~/OneDrive/Bear/Bear --backup ~/OneDrive/Bear/Backup
+# Define paths
+export_dir=~/OneDrive/Bear/Bear
+backup_dir=~/OneDrive/Bear/Backup
+script_path=~/Github/Bear-Markdown-Export/bear_export_sync.py
 
+# Check for required directories
+if [[ ! -d "$export_dir" || ! -d "$backup_dir" ]]; then
+  echo "Error: Required directories do not exist."
+  exit 1
+fi
+
+# Execute the Python script
+if python3 "$script_path" --out "$export_dir" --backup "$backup_dir"; then
+  echo "Export successful."
+  # print the number of notes exported and path to the export directory
+  echo "Exported $(ls -1 "$export_dir" | wc -l) notes to $export_dir"
+else
+  echo "Error: Export failed."
+  exit 1
+fi
