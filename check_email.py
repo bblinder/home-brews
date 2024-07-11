f419f9265af737053890fd8ee0bd0bc09bb7a196
bblinder-splunk <bblinderman@splunk.com>
2024-07-11 13:56:26 -0400

    modified:   check_email.py

--- check_email.py
+++ check_email.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 
-"""Check email address for validity and suggest changes if invalid"""
+"""A small utility to check email address for validity"""
 
+import re
+import argparse
 from email_validator import EmailNotValidError, validate_email
 from rich.console import Console
 
 console = Console()
 
 
-# def suggest_changes(email):
-#     """Suggest syntax changes to email address"""
+def suggest_changes(email):
+    """Suggest syntax changes to email address using regex"""
 
-#     suggestions = []
-#     if "@" not in email:
-#         suggestions.append("::: add @")
-#     if "." not in email:
-#         suggestions.append("::: add .")
-#     if " " in email:
-#         suggestions.append("::: remove spaces")
-#     if ".co" in email:
-#         suggestions.append("::: Try changing .co to .com")
-#     if suggestions:
-#         console.print("Suggestions:", style="bold")
-#         for suggestion in suggestions:
-#             console.print(f"  {suggestion}", style="bold")
-#             return suggestions
-#     else:
-#         return None
+    suggestions = []
+    if "@" not in email:
+        suggestions.append("::: add @")
+    if "." not in email:
+        suggestions.append("::: add .")
+    if " " in email:
+        suggestions.append("::: remove spaces")
+    if ".co" in email:
+        suggestions.append("::: Try changing .co to .com")
+    if ".io" in email and ".com" not in email:
+        suggestions.append("::: Try changing .io to .com")
+    if ".me" in email and ".com" not in email:
+        suggestions.append("::: Try changing .me to .com")
+    if ".us" in email and ".com" not in email:
+        suggestions.append("::: Try changing .us to .com")
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        suggestions.append("::: Invalid format, please check your email address")
+    if len(suggestions) > 0:
+        return suggestions
+    else:
+        return None
 
 
 def validate_address(email):
@@ -46,14 +53,15 @@ def main():
     """Checks email address and suggests changes if invalid"""
 
     email = args.email.strip()
-    validate_address(email)
-    # if validate_address(email) is None:
-    #     suggest_changes(email)
+    if validate_address(email) is None:
+        suggestions = suggest_changes(email)
+        if suggestions:
+            console.print("Suggestions:", style="bold")
+            for suggestion in suggestions:
+                console.print(f"  {suggestion}", style="bold")
 
 
 if __name__ == "__main__":
-    import argparse
-
     parser = argparse.ArgumentParser()
     parser.add_argument("email", help="email address")
     args = parser.parse_args()
