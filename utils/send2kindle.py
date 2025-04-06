c0ce0fabfef6cca94c240c42d19ad541c7371d54
bblinder <filmmaven@gmail.com>
2025-04-06 12:19:35 -0400

    modified:   send2kindle.py

--- utils/send2kindle.py
+++ utils/send2kindle.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = [
+#     "halo",
+#     "python-dotenv",
+#     "redmail",
+# ]
+# ///
 
 """
 A script to send PDFs to a Kindle.
